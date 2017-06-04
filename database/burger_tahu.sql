6d623513ceb1a89f44e511833108832875a936f7
reksarw <reksarw@gmail.com>
2017-06-04 12:16:02 +0700

    Repair database

--- database/burger_tahu.sql
+++ database/burger_tahu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 17, 2017 at 09:18 PM
+-- Generation Time: Jun 04, 2017 at 12:15 PM
 -- Server version: 5.7.18-0ubuntu0.16.04.1
 -- PHP Version: 7.0.15-0ubuntu0.16.04.4
 
@@ -35,13 +35,6 @@ CREATE TABLE `m_admin` (
   `tanggal` date NOT NULL DEFAULT '0000-00-00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `m_admin`
---
-
-INSERT INTO `m_admin` (`id`, `nama`, `username`, `password`, `key`, `tanggal`) VALUES
-(1, 'Reksa Rangga Wardhana', 'reksarw', 'be1265e3c931c1466c929739c7e563b1', '0404e54997-srO-1976394941-LDQM4', '2017-05-16');
-
 -- --------------------------------------------------------
 
 --
@@ -57,14 +50,6 @@ CREATE TABLE `m_kurir` (
   `tanggal` date DEFAULT '0000-00-00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `m_kurir`
---
-
-INSERT INTO `m_kurir` (`id`, `nama`, `username`, `password`, `key`, `tanggal`) VALUES
-(1, 'Reksa Rangga Wardhana', 'reksarangga', 'ead27a029d4e9dbf64b6b397f20b8b66', 'c117b01a42-vSQ-1786394941-Y9XOz', '2017-05-16'),
-(2, 'Rangga Wardhana', 'ranggawardhana', '735a5a3d7a7bd09739533b9d43a30044', '7b098d6eb0-sUH-3450494941-3VtjE', '2017-05-16');
-
 -- --------------------------------------------------------
 
 --
@@ -74,17 +59,10 @@ INSERT INTO `m_kurir` (`id`, `nama`, `username`, `password`, `key`, `tanggal`) V
 CREATE TABLE `m_menu` (
   `id` int(11) NOT NULL,
   `nama` varchar(50) DEFAULT NULL,
+  `gambar` varchar(255) DEFAULT 'http://s3.amazonaws.com/37assets/svn/765-default-avatar.png',
   `harga` varchar(50) DEFAULT NULL,
   `kategori` enum('Makanan','Minuman') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `m_menu`
---
-
-INSERT INTO `m_menu` (`id`, `nama`, `harga`, `kategori`) VALUES
-(1, 'Menu 1', '15000', 'Makanan'),
-(2, 'Menu 2', '10000', 'Minuman');
 
 -- --------------------------------------------------------
 
@@ -96,17 +74,9 @@ CREATE TABLE `m_order` (
   `id` int(11) NOT NULL,
   `id_user` int(11) DEFAULT NULL,
   `id_kurir` int(11) DEFAULT NULL,
-  `tanggal` date DEFAULT '0000-00-00',
+  `tanggal_waktu` datetime DEFAULT CURRENT_TIMESTAMP,
   `status` int(11) DEFAULT '1' COMMENT '1:new order, 2:accept by kurir, 3:current pengiriman, 4:pengiriman selese, 5:cancel by admin or user'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `m_order`
---
-
-INSERT INTO `m_order` (`id`, `id_user`, `id_kurir`, `tanggal`, `status`) VALUES
-(1, 1, 1, '2017-05-16', 1),
-(2, 1, 2, '2017-05-17', 2);
 
 -- --------------------------------------------------------
 
@@ -120,15 +90,8 @@ CREATE TABLE `m_user` (
   `email` varchar(50) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `key` varchar(150) DEFAULT NULL,
-  `tanggal_buat` date DEFAULT '0000-00-00'
+  `tanggal_buat` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `m_user`
---
-
-INSERT INTO `m_user` (`id`, `nama`, `email`, `password`, `key`, `tanggal_buat`) VALUES
-(1, 'Reksa Rangga Wardhana', 'reksarw@gmail.com', 'be1265e3c931c1466c929739c7e563b1', '1a610f8237-BAb-1486394941-JWUY0', '2017-05-16');
 
 -- --------------------------------------------------------
 
@@ -157,15 +120,6 @@ CREATE TABLE `t_order` (
   `total_harga` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `t_order`
---
-
-INSERT INTO `t_order` (`id`, `id_order`, `id_menu`, `jumlah`, `harga`, `total_harga`) VALUES
-(1, 1, 1, 5, 15000, 75000),
-(2, 1, 2, 5, 10000, 50000),
-(3, 2, 1, 1, 15000, 15000);
-
 -- --------------------------------------------------------
 
 --
@@ -175,16 +129,9 @@ INSERT INTO `t_order` (`id`, `id_order`, `id_menu`, `jumlah`, `harga`, `total_ha
 CREATE TABLE `t_tracking` (
   `id` int(11) NOT NULL,
   `id_kurir` int(11) DEFAULT NULL,
-  `latitude` varchar(150) DEFAULT NULL,
-  `longitude` varchar(150) DEFAULT NULL
+  `latitude` varchar(150) DEFAULT 'nothing',
+  `longitude` varchar(150) DEFAULT 'nothing'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `t_tracking`
---
-
-INSERT INTO `t_tracking` (`id`, `id_kurir`, `latitude`, `longitude`) VALUES
-(1, 1, 'LAT', 'LONG');
 
 --
 -- Indexes for dumped tables
@@ -246,27 +193,27 @@ ALTER TABLE `t_tracking`
 -- AUTO_INCREMENT for table `m_admin`
 --
 ALTER TABLE `m_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `m_kurir`
 --
 ALTER TABLE `m_kurir`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `m_menu`
 --
 ALTER TABLE `m_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `m_order`
 --
 ALTER TABLE `m_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `m_user`
 --
 ALTER TABLE `m_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `tools_value`
 --
@@ -276,12 +223,12 @@ ALTER TABLE `tools_value`
 -- AUTO_INCREMENT for table `t_order`
 --
 ALTER TABLE `t_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `t_tracking`
 --
 ALTER TABLE `t_tracking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
